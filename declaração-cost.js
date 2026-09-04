68c547efd3d7113dea3becab61e6baf3372582ed
julia vic <victoria.ju2089@gmail.com>
2026-09-04 17:39:00 -0300

    add declaração-const.js

---
+++ declaração-cost.js
@@ -0,0 +1,11 @@
+function exemploConst() {
+    const x = 10
+    console.log(x); // 10
+    // x = 20; // isso causará um erro porque x foi declarada como const
+    if (true) {
+        const y = 30
+        console.log(y); //30
+    }
+    //console.log(y); // isso causará um erro porque y não existe fora do bloco
+}
+exemploConst();
