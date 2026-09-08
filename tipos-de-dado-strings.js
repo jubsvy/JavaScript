19665afb13346623c4a3cf24202c41a66a0791f1
julia vic <victoria.ju2089@gmail.com>
2026-09-08 14:51:46 -0300

    add tipos de dados strings

---
+++ tipos-de-dado-strings.js
@@ -0,0 +1,3 @@
+let nome = "Ana"; // string com aspas   duplas
+let sobrenome = 'Silva'; // string com aspas simples
+let saudacao = `Olá, ${nome}!`; // template literal com interpolação de variáveis
