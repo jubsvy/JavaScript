a5c77c7d358ccf0d0c20bff56aa985450fff0cb5
julia vic <victoria.ju2089@gmail.com>
2026-09-08 15:25:00 -0300

    add tipos de dados array

---
+++ tipos-de-dados-array.js
@@ -0,0 +1,3 @@
+let frutas = ["maçã", "banana", "laranja"]; // array de strings 
+let numeros = [1, 2 ,3 , 4, 5]; // array de números
+let misto = [true, "texto", 42]; //array com diferentes tipos de dados
