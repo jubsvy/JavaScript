abbb7eae3442448a0cdeedffc24d6a63badef9f5
julia vic <victoria.ju2089@gmail.com>
2026-09-11 17:26:25 -0300

    add operador atribuicao de modulo

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totalDoces = 10;
+let crianca = 3;
+
+// queremos saber quantos doces sobraram após dividir igualmente
+totalDoces %= criancas;
+
+console.log(totaldoces); // 1 -> pois 10 dividindo por 3 sobra 1
