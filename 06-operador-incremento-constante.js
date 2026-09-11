10e790403a17ea2f51075dc64f894d5add6c244f
julia vic <victoria.ju2089@gmail.com>
2026-09-11 17:18:23 -0300

    add operador atribuição de subtracao

--- 06-operador-incremento-constante.js
+++ 06-operador-incremento-constante.js
@@ -1,3 +1,3 @@
 const pontos = 3.14153;
-pontos++; // isoo gerará um erro porque "pontos" é uma constante e não pode ser reatribulada
+pontos++; // isso gerará um erro porque "pontos" é uma constante e não pode ser reatribuida
 console.log(pontos);
---
+++ operador-atribuicao-subtracao.js
@@ -0,0 +1,3 @@
+let saldo = 1000
+let -= 200; // equivale a saldo = saldo - 200, ou seja, saldo agora é 800
+console.log(saldo);
