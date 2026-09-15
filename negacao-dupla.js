f31f82e3c5bc840091e41db048126a5e45f82095
julia vic <victoria.ju2089@gmail.com>
2026-09-15 17:03:24 -0300

    add negação dupla.js

---
+++ negacao-dupla.js
@@ -0,0 +1,10 @@
+// valores falsy
+console.log(!!0);   //false
+console.log(!!"");  // false
+console.log(!!null);   // false
+
+// valores truthy
+console.log(!!1);  // true
+console.log(!!"texto");  // true
+console.log(!![]);  // true(arrays vazios sao objetos, logo são truthy)
+console.log(!!{});  // true (objetos vazios sao truthy)
